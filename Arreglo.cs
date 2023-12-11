57a9f4a465d3899fef3b75184411977214b34d68
ArturoZamoranoB <arturozamoranobeltran9@gmail.com>
2023-12-11 13:13:25 -0700

    primero

---
+++ Arreglo.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recuperacion1y2
+{
+    public class Arreglo
+    {
+        Producto[] productos = new Producto[3];
+        public void Insertar(Producto valor,int posicion)
+        {
+            if(posicion < -1 || posicion> productos.Length)
+            {
+                Console.WriteLine("Ingrese un número valido");
+            }
+            Console.WriteLine($"El nombre {valor.Nombre} y precio {valor.Precio} se ha ingresado correctamente");
+            productos[posicion] = valor;
+        }
+        public void Mostrar()
+        {
+           for(int i = 0; i < productos.Length; i++)
+           {
+                Console.WriteLine($"Posicion {i}: {productos[i]}");
+           }
+        }
+
+        public void OrderCreciente()
+        {
+            for(int i=0; i < productos.Length - 1; i++)
+            {
+                for(int j=0; j < productos.Length - j + 1; j++)
+                {
+                    if (productos[j]?.Precio > productos[j+1]?.Precio)
+                    {
+                        Producto temp = productos[j];
+                        productos[j] = productos[j+1];
+                        productos[j+1] = temp;
+                    } 
+                }
+            }
+        }
+        public void BuscarPrecio(int buscarPrecio)
+        {
+            bool encontrar = false;
+
+           
+                for (int i = 0; i < productos.Length; i++)
+                {
+                    if (productos[i].Precio == buscarPrecio)
+                    {
+                        Console.WriteLine($"El precion buscado {buscarPrecio} se ha encontrado");
+                        encontrar = true;
+                    }
+                    
+                }
+                if(encontrar == false)
+                {
+                Console.WriteLine("No se encuentra");
+               }
+           
+            
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Recuperacion1y2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Arreglo arreglo = new Arreglo();
+           
+            for(int i =0; i < 3; i++)
+            {
+                Console.WriteLine($"Ingrese el nombre del producto {i + 1}");
+                string producto1 = Console.ReadLine();
+
+                Console.WriteLine($"Ingrese el precio de producto {i + 1}");
+                int precio1 = Convert.ToInt32( Console.ReadLine() );
+
+                Producto InsertarProducto = new Producto(producto1, precio1);
+
+                Console.WriteLine("Ingrese la posición");
+                int posicion = Convert.ToInt32( Console.ReadLine() );
+
+                arreglo.Insertar(InsertarProducto,posicion);
+                
+            }
+            arreglo.Mostrar();
+
+            arreglo.OrderCreciente();
+            Console.WriteLine("La lista de los precios en creciente");
+            arreglo.Mostrar();
+
+            Console.WriteLine("Ingrese el precio a buscar");
+            int precioBuscar = Convert.ToInt32( Console.ReadLine() );
+
+            arreglo.BuscarPrecio(precioBuscar);
+
+        }
+    }
+}
